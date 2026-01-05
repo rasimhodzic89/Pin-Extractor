9791f5672e1d9d14cbe346cb1d4acdca714bdc02
Rasim Hodzic <rasimhodzic89@gmail.com>
2026-01-05 11:25:14 -0500

    Added an if statement to check for enough words in my words variable

--- Pin-Extractor.py
+++ Pin-Extractor.py
@@ -2,13 +2,15 @@ def pin_extractor(poem):
     secret_code = ''
     lines = poem.split('\n')
     for line_index, line in enumerate(lines):
-        print(line_index, line)
         words = line.split()
-        print(words[line_index])
+        
+        if line_index < len(words):
+            secret_code += str(len(words[line_index]))
+    return secret_code
 
 poem = """Stars and the moon
 shine in the sky
 white and bright
 until the end of the night"""
 
-pin_extractor(poem)
+print(pin_extractor(poem))
